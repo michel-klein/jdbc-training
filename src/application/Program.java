53a5f84588bbfd3365d76a6787dd992566182006
Michel Klein <michel.kps@outlook.com>
2023-07-28 17:28:28 -0300

    feat: retrieve data

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,32 @@ package application;
 import db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+        try {
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("select * from department");
+            while(rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("name"));
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
         DB.closeConnection();
     }
 }
